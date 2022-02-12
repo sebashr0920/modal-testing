fb173b439863bce527a6d6521556632ec7504340
Sebastian Hernandez <shernandez@p44-sebastianhernandez.local>
2022-02-11 21:08:05 -0500

    refactor test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,27 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const button = screen.getByText(/open modal/i);
-  fireEvent.click(button);
-  const modal = screen.getByText(/this is a modal/i)
-  expect(modal).toBeInTheDocument();
+describe('App', () => {
+  let button;
+  let modal;
+
+  test('should render only one modal', () => {
+    given_a_button();
+    when_button_is_clicked();
+    then_one_modal_should_be_rendered();
+  });
+
+  function given_a_button() {
+    render(<App/>);
+    button = screen.getByText(/open modal/i);
+  }
+
+  function when_button_is_clicked() {
+    fireEvent.click(button);
+  }
+
+  function then_one_modal_should_be_rendered() {
+    modal = screen.getByText(/this is a modal/i);
+    expect(modal).toBeInTheDocument();
+  }
 });
